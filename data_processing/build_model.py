2c046731b244d62212c0c3ab36cf1d2cc6b9c9b1
Diane Yang <diane@duolingo.com>
2017-02-26 20:29:53 -0500

    Basically finished Harry Potter exercise

--- data_processing/build_model.py
+++ data_processing/build_model.py
@@ -5,11 +5,12 @@ from collections import defaultdict
 abbreviations = ['Ms.', 'Mrs.', 'Mr.', 'St.', 'S.P.E.W.']
 terminal_punctuation = ['.', '?', '!', '."', '?"', '!"']
 replacements = {
-	u'\u201c': '"',
-	u'\u201d': '"',
+	u'\u201c': '',
+	u'\u201d': '',
 	u'\u2018': '\'',
 	u'\u2019': '\'',
 	u'\u00e9': 'e',
+	'\"': '',
 }
 
 def word_ends_sentence(word):
@@ -23,7 +24,7 @@ def word_ends_sentence(word):
 	return False
 
 def generate_text(start_states, data, end_states, order):
-	current_state = random.choice(start_states)
+	current_state = random.choice(start_states.keys())
 	text = current_state.split()
 	while current_state not in end_states:
 		next_word = random.choice(data[current_state].keys())
@@ -36,7 +37,7 @@ def generate_text(start_states, data, end_states, order):
 for n in range(1, 5):
 	# A mapping from a sequence of n words to words that come next and their frequencies.
 	word_stats = defaultdict(lambda: defaultdict(int))
-	start_states = []
+	start_states = defaultdict(int)
 	end_states = []
 	# Process each Harry Potter file
 	file_name_format = 'harry_potter_cleaned/hp%d-cleaned.txt'
@@ -66,20 +67,25 @@ for n in range(1, 5):
 					last_n_words = sentence[k-n+1:k+1]
 					current_state = ' '.join(last_n_words)
 					if k == n-1:
-						start_states.append(current_state)
+						start_states[current_state] += 1
 					if k == len(sentence)-1:
 						end_states.append(current_state)
 					else:
 						next_word = sentence[k+1]
 						word_stats[current_state][next_word] += 1
 
-
 	for i in range(10):
 		print generate_text(start_states, word_stats, end_states, n)
-	print '----------'
+
+	output = {}
+	output['startStates'] = start_states
+	output['endStates'] = end_states
+	output['wordStats'] = word_stats
 
 	# Write results to a json file
 	output_file_name = '../json/hp-order-%d.json' % n
 	with open(output_file_name, 'w') as outfile:
-		json.dump(word_stats, outfile, indent=4, sort_keys=True)
+		outfile.write('var hp%d = ' % n)
+		json.dump(output, outfile, indent=4, sort_keys=True)
 	print "Done with order %d" % n
+	print '----------'
